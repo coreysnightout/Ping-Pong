61f9bea5ca4d6cfd68d10ae6e783ca51277358e2
Corey Redding <coreyredding@Coreys-iMac.local>
2017-05-06 22:30:33 -0700

    edit javascript to return userInput values

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,13 @@
-////Business Logic////
-var pingPong = function(form) {
-
-  var output = "";
-  for(var index = 1; index <= form; index += 1) {
-
-    if (index % 3 === 0){
-      output += "ping" + " ";
-    } else if (index % 5 === 0){
-      output += "pong" + " ";
-    } else if (index % 15 === 0){
-      output += "ping" + " ";
-    } else {
-      output += index + " "
-    }
-  }
-
-  return output;
-
-};
+var pingPong = function(userInput)
 
 
-
-////User Interface Logic////
 $(document).ready(function() {
-  ("form#ping-pong").submit(event){
+  $(".ping-pong").submit(function(event){
     event.preventDefault();
-    var count = parseInt($("input#number").val());
-    var result = pingPong(count);
+    var userInput = parseInt($("input#number").val());
+    var output = pingPong(userInput);
+    console.log(userInput);
 
-  }
+    $("#output").append(userInput);
+  });
 });
