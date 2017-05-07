8ddbbbb1b099eea24989415c058e0c39835234ba
Corey Redding <coreyredding@Coreys-iMac.local>
2017-05-06 23:43:45 -0700

    add for each loop into UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
-var pingPong = function(userInput)
+// Back End //
+var pingPong = function(input) {
+  var output = [];
+  for(var index = 1; index <= input; index++){
+  output.push(index);
+  }
+  return output;
+};
 
 
+
+
+// UI Logic //
 $(document).ready(function() {
   $(".ping-pong").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("input#number").val());
-    var output = pingPong(userInput);
-    console.log(userInput);
-
-    $("#output").append(userInput);
+    var result = pingPong(userInput);
+    console.log(result);
+    // console.log(userInput);
+    result.forEach(function(listItem) {
+      $("#output").append("<li>" + listItem + "</li>");
+    });
   });
 });
